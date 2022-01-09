680ff592e4d46ed6d83a9498a38db6decc392405
Jonathan Thom <jonathanthom@hey.com>
2022-01-09 13:16:39 -0800

    Will declare winner or loser

--- lib/wordle.rb
+++ lib/wordle.rb
@@ -4,5 +4,45 @@ require_relative "wordle/version"
 
 module Wordle
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Game
+    def self.play
+      new.play
+    end
+
+    def initialize
+      @target_word = generate_word
+    end
+
+    def play
+      winner = false
+      attempts = 0
+
+      puts "Guess: "
+
+      while attempts < 6 && !winner
+        guess = gets.chomp
+
+        if guess.downcase.strip === @target_word
+          winner = true
+          break
+        end
+
+        attempts += 1
+      end
+
+      if winner
+        puts "winner!"
+      else
+        puts "better luck next time!"
+      end
+    end
+
+    private
+
+    def generate_word
+      # maybe its own class
+      "gorge"
+    end
+  end
 end
