4fedc75ffeea4cd62f9cf82bebf7fcf5f124473f
Jonathan Thom <jonathanthom@hey.com>
2022-01-09 14:15:41 -0800

    Generates random 5 letter word

--- lib/wordle/list.rb
+++ lib/wordle/list.rb
@@ -3,14 +3,25 @@
 module Wordle
   class List
     def initialize(length = 5)
+      @length = length
     end
 
     def random
-      "gorge"
+      list.sample.downcase.strip
     end
 
     def invalid?(word)
       false
+    end
+
+    private
+
+    def list
+      @_list ||= begin
+        yes = File.readlines("/usr/share/dict/words").select do |word|
+          word.strip.length == @length
+        end
+      end
     end
   end
 end
