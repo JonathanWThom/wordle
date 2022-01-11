7b0dc99c032b9dfad4eea21fbde39ffeb8c0b0aa
Jonathan Thom <jonathanthom@hey.com>
2022-01-10 21:19:41 -0800

    rake standard fix

--- lib/wordle/legend.rb
+++ lib/wordle/legend.rb
@@ -15,15 +15,15 @@ module Wordle
     private
 
     def green_example
-      "#{'w'.green}eary - The letter w is in the word and in the correct spot."
+      "#{"w".green}eary - The letter w is in the word and in the correct spot."
     end
 
     def yellow_example
-      "p#{'i'.yellow}lls - The letter i is in the word but in the wrong spot."
+      "p#{"i".yellow}lls - The letter i is in the word but in the wrong spot."
     end
 
     def gray_example
-      "vag#{'u'.gray}e - The letter u is not in the word in any spot."
+      "vag#{"u".gray}e - The letter u is not in the word in any spot."
     end
   end
 end
